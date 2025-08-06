c0edd19a82342788a6cdbfa6f4669b012a895165
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2025-08-06 18:04:48 +0600

    setup project and added nevber and footer

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+
+const About = () => {
+    return (
+        <div className=" my-5">
+            <h1 className="text-2xl font-bold">this is about page </h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi ut quo eius expedita tempora cum harum illum natus rem dignissimos nam, aut sed tempore incidunt accusamus assumenda sapiente, laborum neque?
+            Obcaecati, nobis maiores tenetur eaque sunt vero laborum vel. </p>
+        </div>
+    );
+};
+
+export default About;
